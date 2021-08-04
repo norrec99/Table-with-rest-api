55a794f3cf9ab7a58e9066342f5f9b2030758894
cagatay soyer <cagataysoyer@gmail.com>
2021-08-04 15:02:45 +0300

    changed icon and color, added checkbox for completed

--- src/components/EditModal.vue
+++ src/components/EditModal.vue
@@ -17,18 +17,22 @@
               </button>
             </div>
             <div class="sm:flex sm:items-start">
-              <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                <ExclamationIcon class="h-6 w-6 text-red-600" aria-hidden="true" />
+              <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10">
+                <SwitchHorizontalIcon class="h-6 w-6 text-indigo-600" aria-hidden="true" />
               </div>
               <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900"> Deactivate account </DialogTitle>
+                <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900"> Title </DialogTitle>
                 <div class="mt-2">
                   <input v-model="title" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+                </div>
+                <div class="mt-2">
+                  <input type="checkbox" class="form-checkbox" v-model="isCompleted" />
+                  <span class="ml-2">Completed</span>
                 </div>
               </div>
             </div>
             <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-              <button @click="saveTodo(result.id)" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Save</button>
+              <button @click="saveTodo(result.id)" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Save</button>
               <button @click="open ? true : false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm">Cancel</button>
             </div>
           </div>
@@ -42,7 +46,7 @@
 import { ref } from 'vue';
 import axios from 'axios';
 import { Dialog, DialogOverlay, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue';
-import { ExclamationIcon, XIcon } from '@heroicons/vue/outline';
+import { SwitchHorizontalIcon, XIcon } from '@heroicons/vue/outline';
 
 export default {
   props: {
@@ -57,7 +61,7 @@ export default {
     DialogTitle,
     TransitionChild,
     TransitionRoot,
-    ExclamationIcon,
+    SwitchHorizontalIcon,
     XIcon
   },
   emits: ['updateItem'],
@@ -66,10 +70,11 @@ export default {
     // eslint-disable-next-line vue/no-setup-props-destructure
     const result = props.todo;
     const title = ref(result.title);
+    const isCompleted = ref(result.completed);
 
     async function saveTodo(id) {
       await axios
-        .patch(`https://jsonplaceholder.typicode.com/todos/${id}`, { title: title.value })
+        .patch(`https://jsonplaceholder.typicode.com/todos/${id}`, { title: title.value, completed: isCompleted.value })
         .then(function (response) {
           emit('updateItem', response.data);
           open.value = false;
@@ -83,7 +88,8 @@ export default {
       open,
       title,
       saveTodo,
-      result
+      result,
+      isCompleted
     };
   }
 };
