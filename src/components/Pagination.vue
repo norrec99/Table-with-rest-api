741b613ce1f8567b96f8bb254ca20f49f96e63c6
cagatay soyer <cagataysoyer@gmail.com>
2021-08-04 15:07:10 +0300

    cursor-pointer added

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,13 +1,13 @@
 <template>
   <nav class="border-t border-gray-200 px-4 flex items-center justify-between sm:px-0">
     <div class="-mt-px w-0 flex-1 flex">
-      <a @click="goPreviousPage" class="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
+      <a @click="goPreviousPage" class="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 cursor-pointer">
         <ArrowNarrowLeftIcon class="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
         Previous
       </a>
     </div>
     <div class="hidden md:-mt-px md:flex">
-      <a v-for="pageNumber in pageNumbers" :key="pageNumber" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"> {{ pageNumber }} </a>
+      <a v-for="pageNumber in pageNumbers" :key="pageNumber" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium cursor-pointer"> {{ pageNumber }} </a>
 
       <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" -->
       <!-- <a href="#" class="border-indigo-500 text-indigo-600 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium" aria-current="page"> 2 </a>
@@ -18,7 +18,7 @@
       <a href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"> 10 </a> -->
     </div>
     <div class="-mt-px w-0 flex-1 flex justify-end">
-      <a @click="goNextPage" class="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
+      <a @click="goNextPage" class="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 cursor-pointer">
         Next
         <ArrowNarrowRightIcon class="ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
       </a>
